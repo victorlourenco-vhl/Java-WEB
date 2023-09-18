320ca00660cd754d5d9e94bb9ee92b1d0ca75748
Victor Lourenço <victorhlo12@hotmail.com>
2023-09-18 14:18:49 -0300

    FEAT: Criando a página agenda.jsp e removendo os cometários do Controller

--- src/main/java/com/agenda/controllers/Controller.java
+++ src/main/java/com/agenda/controllers/Controller.java
@@ -10,9 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.agenda.models.DAO;
 
-/**
- * Servlet implementation class Controller
- */
 @WebServlet(urlPatterns = {"/Controller", "/main"})
 public class Controller extends HttpServlet {
 	
@@ -20,28 +17,16 @@ public class Controller extends HttpServlet {
 	
 	DAO dao = new DAO();
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
     public Controller() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 		dao.testConnection();
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
---
+++ src/main/webapp/agenda.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+<meta charset="UTF-8">
+<title>Agenda de contatos</title>
+
+<link rel="icon" href="imagens/chamada-telefonica.png" />
+<link rel="stylesheet" href="style.css" />
+</head>
+<body>
+	<div class="container">
+		<h1>Lista de Contatos</h1>
+		<a href="" class="botao">Adicionar</a>
+	</div>
+</body>
+</html>
